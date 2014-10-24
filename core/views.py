15d963d2dd4a2e033ffc1a72968b72d418463152
Gustavo Correia <correia.gustavol@gmail.com>
2014-10-24 16:59:27 -0200

    settings

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.template import Context, loader
 from core.models import Adult, Pool
 from django.http import HttpResponse
 
+
 def index(request):
     adult_list = Adult.objects.all().order_by('name')
     t = loader.get_template('core/base.html')
@@ -9,3 +10,5 @@ def index(request):
         'adult_list': adult_list,
     })
     return HttpResponse(t.render(c))
+
+
