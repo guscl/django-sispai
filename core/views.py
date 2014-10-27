652cd69293976f496b669e96fdcaf969ce5e12c4
rodrigorincon <rodrigolr_15@hotmail.com>
2014-10-27 10:44:12 -0200

    mudando o texto do botao ligar lampada ao clicar

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.template import Context, loader
 from core.models import Adult, Pool
 from django.http import HttpResponse
 
+status_ativacao = False
 
 def index(request):
     adult_list = Adult.objects.all().order_by('name')
@@ -10,5 +11,18 @@ def index(request):
         'adult_list': adult_list,
     })
     return HttpResponse(t.render(c))
+
+def alterStatus(request):
+	global status_ativacao
+	status_ativacao = not status_ativacao
+	print status_ativacao
+	t = loader.get_template('core/base.html')
+	c = Context({
+		'ativado': status_ativacao,
+	})
+	return HttpResponse(t.render(c))
+
+	
+
 
 
--- sispai/urls.py
+++ sispai/urls.py
@@ -5,7 +5,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^core/$', 'core.views.index'),   
+    url(r'^core/$', 'core.views.index'),
+    url(r'^core/change/$', 'core.views.alterStatus'),   
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
